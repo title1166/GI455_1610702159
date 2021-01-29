978d07889c1e7ef117b9011d58f9573fdcde24fd
unknown <title11667@gmail.com>
2021-01-29 18:58:21 +0700

    send assingment_1

---
+++ homework week1/Assets/assignment_1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class assignment_1 : MonoBehaviour
+{
+    public string n;
+    public GameObject inputnakah;
+    public GameObject outputnakub;
+    public void assignmenteieizahahaplus()
+    {
+
+        n = inputnakah.GetComponent<Text>().text;
+        if (n == "cold" || n == "hot" || n == "burn" || n == "freeze" || n == "cool")
+        {
+            outputnakub.GetComponent<Text>().text = n+ " is found";
+        }
+
+        else
+        {
+            outputnakub.GetComponent<Text>().text = n+ " is not found";
+        }
+
+    }
+}
